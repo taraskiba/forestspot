48011bcf42df4cb87f0ca94475c981c008731993
Tara Skiba <tskiba@vols.utk.edu>
2026-06-22 10:32:15 -0400

    fix radius randomization to have equal probability of sampling

--- skiba/buffer_coordinates.py
+++ skiba/buffer_coordinates.py
@@ -160,7 +160,9 @@ class BufferCoordinates:
 
         # Randomize the point's location within the circle
         angle = np.random.uniform(0, 2 * np.pi)
-        distance = np.random.uniform(0, radius_m)
+        distance = (
+            np.random.uniform(0, 1) * radius_m
+        )  # Random distance from center up to radius
         # Calculate center of the circle so that the point is inside the circle but not at the center
         center_x = x - distance * np.cos(angle)
         center_y = y - distance * np.sin(angle)
